e7fccb40dfc6e0092e47bb818666dbff89d0fae7
Flera <Izzi_113@mail.ru>
2022-08-11 00:57:43 +0400

    Lesson4

---
+++ Task01/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, которая покажет
+// количество чётных чисел в массиве.
+
+int[] CreateRandomArray( int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+
+    for(int i=0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+} 
+
+int[] CreateArray(int size)
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i +1} element of {size}: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int EvenNum(int[] array)
+{
+    int even = 0;
+
+    for(int i = 0; i < array.Length; i++ )
+    {
+        if(array[i]%2==0)
+        {
+            //return array[i];
+            even++;
+        }
+
+    }
+return even;
+}
+
+
+int[] arr = CreateRandomArray(5, 100, 1000);
+int result = EvenNum(arr);
+ShowArray(arr);
+Console.WriteLine(result);
---
+++ Task02/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateRandomArray( int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+
+    for(int i=0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+} 
+
+int[] CreateArray(int size)
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i +1} element of {size}: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int SumNonEvenNum(int[] array)
+{
+    int sum = 0;
+
+    for(int i = 0; i < array.Length; i++ )
+    {
+        if(i%2!=0)
+        {
+            sum += array[i];
+        }
+
+    }
+return sum;
+}
+
+int[] arr = CreateRandomArray(5, 100, 1000);
+int result = SumNonEvenNum(arr);
+ShowArray(arr);
+Console.WriteLine(result);
